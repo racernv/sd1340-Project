43432288900e0f309aa170a0a1effb6e38f739b1
Jay Crabill <crabill87@gmail.com>
2014-08-11 06:31:53 -0500

    Finished Application.js for Length Page

--- js/application.js
+++ js/application.js
@@ -1,12 +1,3 @@
 function lengthconversion() {
- var unitOne
- var unitOneType
- var unitTwoType
- var result
-
- unitOne = $('#firstUnit').val();
- unitOneType = $('#firstUnitType').val();
- unitTwoType = $('#secondUnitType').val();
- 
- $('#result').val(unitOne + unitOneType);
+ $('#result').val(math.eval($('#firstUnit').val() + " " + $('#firstUnitType').val() + " to " + $('#secondUnitType').val()).valueOf());
 }
