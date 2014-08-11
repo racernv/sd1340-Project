ea364164c173bc3e978eba088af969fd10705d9c
Jay Crabill <crabill87@gmail.com>
2014-08-11 08:44:33 -0500

    Mass Page html/js completed, fixed Alignment

--- js/application.js
+++ js/application.js
@@ -1,3 +1,7 @@
 function lengthconversion() {
  $('#result').val(math.eval($('#firstUnit').val() + " " + $('#firstUnitType').val() + " to " + $('#secondUnitType').val()).valueOf());
 }
+
+function massconversion() {
+ $('#massResult').val(math.eval($('#firstMass').val() + " " + $('#firstMassType').val() + " to " + $('#secondMassType').val()).valueOf());
+}
