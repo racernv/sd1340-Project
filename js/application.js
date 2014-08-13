404a37934d873de01eeacd92358efa1676e49622
Jay Crabill <crabill87@gmail.com>
2014-08-13 00:31:03 -0500

    Finished script for temp page

--- js/application.js
+++ js/application.js
@@ -5,3 +5,15 @@ function lengthconversion() {
 function massconversion() {
  $('#massResult').val(math.eval($('#firstMass').val() + " " + $('#firstMassType').val() + " to " + $('#secondMassType').val()).valueOf());
 }
+
+function tempconversion() {
+var status = $('#tempConv').val();
+
+if (status === "f") { 
+	 $('#tempResult').val(math.eval($('#temperature').val() + " degF to degC"));
+	}
+else {
+	 $('#tempResult').val(math.eval($('#temperature').val() + " degC to degF"));
+	}
+
+}
