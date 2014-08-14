f00a6e1b443a8b40d79d9b2bf7a553ce82068cd2
Jay Crabill <crabill87@gmail.com>
2014-08-14 12:05:34 -0500

    Refactored Home and Length Page, Removed old footer script

--- js/application.js
+++ js/application.js
@@ -27,18 +27,3 @@ function tempconversion() {
 	}
 
 }
-
-
-$(window).bind('resize',function(event){
-	window.scrollTo(0,0);
-	var winhigh = $.mobile.getScreenHeight(); //Get available screen height, not including any browser chrome
-	var headhigh = $('[data-role="header"]').first().outerHeight(); //Get height of first page's header
-	var foothigh = $('[data-role="footer"]').first().outerHeight(); //Get height of first page's header
-	var $content=$('[data-role="content"]');
-	var contentpaddingwidth=parseInt($content.css("padding-left").replace("px", ""))+parseInt($('[data-role="content"]').css("padding-right").replace("px", ""));
-	var contentpaddingheight=parseInt($content.css("padding-top").replace("px", ""))+parseInt($('[data-role="content"]').css("padding-bottom").replace("px", ""));
-	winhigh = winhigh - headhigh - foothigh - contentpaddingheight; 
-	winwide = $(document).width(); //Get width of document
-	winwide = winwide - contentpaddingwidth; 
-	$content.css('width',winwide + 'px').css('height',winhigh + 'px'); //Change div to maximum visible area
-});
