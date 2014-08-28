c348bac5de2f97839d1066fcf241c78e7169f622
Jay Crabill <crabill87@gmail.com>
2014-08-28 15:44:55 -0500

    Fixed buttons, and Added Number checker

--- js/application.js
+++ js/application.js
@@ -1,18 +1,40 @@
 function lengthconversion() {
-	var ans = math.eval($('#firstUnit').val() + " " + $('#firstUnitType').val() + " to " + $('#secondUnitType').val());
-	$('#result').val(math.format(ans, {precision: 10, notation: 'fixed'}));
+	var checker = $('#firstUnit').val();
+	if (!isNaN(checker) && checker) {
+		var ans = math.eval($('#firstUnit').val() + " " + $('#firstUnitType').val() + " to " + $('#secondUnitType').val());
+		$('#result').val(math.format(ans, {precision: 10, notation: 'fixed'}));
+		}
+	
+	else {
+		alert('You have either not entered a value, or entered an illegal character.');
+	}
 }
 function massconversion() {
+	var checker = $('#firstMass').val();
+	if (!isNaN(checker) && checker) {
 	var ans = math.eval($('#firstMass').val() + " " + $('#firstMassType').val() + " to " + $('#secondMassType').val());
 	$('#massResult').val(math.format(ans, {precision: 14}));
+			}
+	else {
+		alert('You have either not entered a value, or entered an illegal character.');
+	}
 }
 
 function liquidconversion() {
+	var checker = $('#firstLiquid').val();
+	if (!isNaN(checker) && checker) {
 	var ans = math.eval($('#firstLiquid').val() + " " + $('#firstLiquidType').val() + " to " + $('#secondLiquidType').val());
 	$('#liquidResult').val(math.format(ans, {precision: 14}));
+	}
+	else {
+		alert('You have either not entered a value, or entered an illegal character.');
+	}
 }
 
 function tempconversion() {
+	var checker = $('#temperature').val();
+	if (!isNaN(checker) && checker) {
+
 	var status = $('#tempConv').val();
 
 	if (status === "f") { 
@@ -24,6 +46,10 @@ function tempconversion() {
 	var ans = math.eval($('#temperature').val() + " degC to degF");
 	
 	 $('#tempResult').val(math.format(ans, {precision: 10, notation: 'fixed'}));
+	}
+	}
+	else {
+		alert('You have either not entered a value, or entered an illegal character.');
 	}
 
 }
